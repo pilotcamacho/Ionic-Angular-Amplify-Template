e28af1a53059fa8315869f232b5e0f1bfc6010f6
Fernando Camacho <fernando.camacho@peopleart.co>
2024-10-05 18:51:02 -0500

    Try to view ToDo as the example did in Angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgFor, CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from '../../../amplify/data/resource';
+
+const client = generateClient<Schema>();
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, NgFor, CommonModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  constructor() { }
+
+  signOut() {
+    console.log('signOut')
+  }
+
+  todos: any[] = [];
+
+  ngOnInit(): void {
+    this.listTodos();
+  }
+
+  listTodos() {
+    try {
+      client.models.Todo.observeQuery().subscribe({
+        next: ({ items, isSynced }) => {
+          this.todos = items;
+        },
+      });
+    } catch (error) {
+      console.error('error fetching todos', error);
+    }
+  }
+
+  createTodo() {
+    try {
+      client.models.Todo.create({
+        content: window.prompt('Todo content'),
+      });
+      this.listTodos();
+    } catch (error) {
+      console.error('error creating todos', error);
+    }
+  }
+  
+  deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+
 }
